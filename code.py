a3170588a31c24d479036a6dd29f0da11b781fa0
Wouter <woutervantol@hotmail.nl>
2022-11-09 13:14:03 +0100

    paar kleine aanpassingen

--- code.py
+++ code.py
@@ -11,15 +11,7 @@ n_stars = 1000
 alpha_IMF = -2.35
 from amuse.ext.salpeter import new_salpeter_mass_distribution
 m_stars = new_salpeter_mass_distribution(n_stars, 0.1|units.MSun, 100|units.MSun, alpha_IMF)
-
-
-#plot the initial mass distribution in loglog
-# hist, bins = np.histogram(m_stars.value_in(units.MSun), bins=20)
-# logbins = np.logspace(np.log10(bins[0]),np.log10(bins[-1]),len(bins))
-# plt.hist(m_stars.value_in(units.MSun), bins=logbins, density=True)
-# plt.xscale('log')
-# plt.show()
-
+total_mass = np.sum(m_stars)
 
 r_cluster = 1.0 | units.parsec
 
@@ -30,27 +22,26 @@ from amuse.ic.plummer import new_plummer_sphere
 bodies=new_plummer_sphere(n_stars, convert_nbody=converter)
 bodies.scale_to_standard(converter)
 
-# plt.scatter(bodies.x.value_in(units.parsec), bodies.y.value_in(units.parsec))
-# plt.axis("equal")
-# plt.show()
+# print(dir(bodies))
+
 
 from amuse.community.ph4.interface import ph4
 gravity = ph4(converter)
 gravity.particles.add_particles(bodies)
+print("gravitational energy: ", gravity.get_total_energy())
+print("kinetic energy: ", bodies.kinetic_energy)
 
 channel = gravity.particles.new_channel_to(bodies)
 
-times = np.arange(0, 100, 10) | units.Myr
+times = np.arange(0, 9, 1) | units.Myr
 figs, axes = plt.subplots(3, 3, sharex=True, sharey=True)
 axes = axes.flatten()
 import seaborn as sns
 for i, t in enumerate(tqdm(times)):
     gravity.evolve_model(t)
     channel.copy()
-    if i%10 == 0 and i <91:
-        sns.kdeplot(x=bodies.x.value_in(units.parsec), y=bodies.y.value_in(units.parsec), ax=axes[i//10])
-    # axes[i].scatter(bodies.x.value_in(units.parsec), bodies.y.value_in(units.parsec))
+    # sns.kdeplot(x=bodies.x.value_in(units.parsec), y=bodies.y.value_in(units.parsec), ax=axes[i])
+    axes[i].scatter(bodies.x.value_in(units.parsec), bodies.y.value_in(units.parsec), s=1, c=np.log(m_stars.value_in(units.MSun)))
     # plt.axis("equal")
 plt.show()
 gravity.stop()
-

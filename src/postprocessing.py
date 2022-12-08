5944e3522d19fa14388e3e85d5fcfe1f42068f9a
Wouter <woutervantol@hotmail.nl>
2022-12-08 14:05:37 +0100

    added data/figures, removed deprecated code

---
+++ src/postprocessing.py
@@ -0,0 +1,132 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from amuse.units import units
+from matplotlib import animation
+
+filestring = ""
+filestring = "_ratio{}_run{}".format(5, "test")
+
+kinetic_energies = np.load("./data/kinetic_energy{}.npy".format(filestring), allow_pickle=True)
+potential_energies = np.load("./data/potential_energy{}.npy".format(filestring), allow_pickle=True)
+times = np.load("./data/times{}.npy".format(filestring), allow_pickle=True)
+gaspositions = np.load("./data/gaspositions{}.npy".format(filestring), allow_pickle=True)
+gasvelocities = np.load("./data/gasvelocities{}.npy".format(filestring), allow_pickle=True)
+starpositions = np.load("./data/starpositions{}.npy".format(filestring), allow_pickle=True)
+starvelocities = np.load("./data/starvelocities{}.npy".format(filestring), allow_pickle=True)
+
+#find time at which supernova starts
+SNtime = 0 | units.Myr
+for i in range(len(gasvelocities)):
+    if len(gasvelocities[i]) > len(gasvelocities[0]):
+        SNtime = times[i]
+        break
+
+
+def plot_avg_velocities():
+    plt.figure()
+    vels = []
+    for frame in range(len(gasvelocities)):
+        temp = np.sqrt(gasvelocities[frame][:, 0].value_in(units.ms)**2 + gasvelocities[frame][:, 1].value_in(units.ms)**2 + gasvelocities[frame][:, 2].value_in(units.ms)**2)
+        relevant = temp[temp < 4000]
+        vels.append(np.average(relevant))
+    plt.title("Gas velocity")
+    plt.xlabel("Time (Myr)")
+    plt.ylabel("Average gas velocity (m/s)")
+    plt.vlines(SNtime, np.min(vels), np.max(vels), color="black", linestyle="dashed")
+    plt.plot(times, vels)
+    plt.savefig("./figures/avg_vel.png")
+
+def plot_avg_positions():
+    plt.figure()
+    poss = []
+    for frame in range(len(gasvelocities)):
+        temp = np.sqrt(gaspositions[frame][:, 0].value_in(units.parsec)**2 + gaspositions[frame][:, 1].value_in(units.parsec)**2 + gaspositions[frame][:, 2].value_in(units.parsec)**2)
+        relevant = temp[temp < 10]
+        poss.append(np.average(relevant))
+    plt.title("Gas position")
+    plt.xlabel("Time (Myr)")
+    plt.ylabel("Average gas position (m/s)")
+    plt.vlines(SNtime, np.min(poss), np.max(poss), color="black", linestyle="dashed")
+    plt.plot(times, poss)
+    plt.savefig("./figures/avg_pos.png")
+
+def plot_energies():
+    plt.figure()
+    plt.semilogy(times, potential_energies, label="potential energy")
+    plt.semilogy(times, kinetic_energies, label="kinetic energy")
+    plt.title("Energy")
+    plt.xlabel("Time (Myr)")
+    plt.ylabel("Energy (m^2 kg s^-2)")
+    plt.vlines(SNtime, np.min(kinetic_energies), np.max(kinetic_energies), color="black", linestyle="dashed")
+    plt.legend()
+    plt.savefig("./figures/energy.png")
+
+def plot_relative_energy():
+    plt.figure()
+    data = kinetic_energies / potential_energies
+    plt.semilogy(times, data)
+    plt.vlines(SNtime, np.min(data), np.max(data), color="black", linestyle="dashed")
+    plt.title("boundness energy")
+    plt.xlabel("Time (Myr)")
+    plt.ylabel("Energy ratio")
+    plt.legend()
+    plt.savefig("./figures/energy_rel.png")
+
+def make_vel_hist(frame):
+    print(frame)
+    ax.cla()
+    ax.set_title("Time: {:2f} Myr".format(times[frame]))
+    ax.set_xlabel("Absolute speed (m/s)")
+    ax.set_ylabel("Nr of gas particles")
+    ax.set_xlim(0, 10000)
+    ax.set_ylim(0, 200)
+    ax.hist(np.sqrt(gasvelocities[frame][:, 0].value_in(units.ms)**2 + gasvelocities[frame][:, 1].value_in(units.ms)**2 + gasvelocities[frame][:, 2].value_in(units.ms)**2), bins=100, range=(0, 10000))
+
+def make_pos_hist(frame):
+    print(frame)
+    ax.cla()
+    ax.set_title("Time: {:2f} Myr".format(times[frame]))
+    ax.set_xlabel("Position from center (parsec)")
+    ax.set_ylabel("Nr of gas particles")
+    ax.set_xlim(0, 10)
+    ax.set_ylim(0, 200)
+    ax.hist(np.sqrt(gaspositions[frame][:, 0].value_in(units.parsec)**2 + gaspositions[frame][:, 1].value_in(units.parsec)**2 + gaspositions[frame][:, 2].value_in(units.parsec)**2), bins=100, range=(0, 10))
+
+def make_animation(frame):
+    print(frame)
+    ax.cla()
+    ax.set_title("Time: {:.2f} Myr".format(times[frame]))
+    ax.set_xlim(-5, 5)
+    ax.set_ylim(-5, 5)
+    ax.set_xlabel("parsec")
+    ax.set_ylabel("parsec")
+    
+    #stupid amuse quantities dont work so we have to iterate by hand to convert units
+    starpos = np.zeros((len(starpositions[0]), 3))
+    for i in range(len(starpositions[frame])):
+        starpos[i,0] = starpositions[frame][i,0].value_in(units.parsec)
+        starpos[i,1] = starpositions[frame][i,1].value_in(units.parsec)
+        starpos[i,2] = starpositions[frame][i,2].value_in(units.parsec)
+    ax.scatter(gaspositions[frame][:,0].value_in(units.parsec), gaspositions[frame][:,1].value_in(units.parsec), s=0.5, label="gas")
+    ax.scatter(starpos[:,0], starpos[:,1], s=3, label="stars")
+    # ax.scatter(bodies[np.argmax(bodies.mass)].x.value_in(units.parsec), bodies[np.argmax(bodies.mass)].y.value_in(units.parsec), s=5, c="red")
+
+# fig, ax = plt.subplots()
+# anim = animation.FuncAnimation(fig, make_animation, frames=len(times))
+# writer = animation.FFMpegWriter(fps=len(gasvelocities)/10.)
+# anim.save("./figures/animation.mp4", writer=writer)
+
+# fig, ax = plt.subplots()
+# anim = animation.FuncAnimation(fig, make_pos_hist, frames=len(times))
+# writer = animation.FFMpegWriter(fps=len(gasvelocities)/10.)
+# anim.save("./figures/pos_hist.mp4", writer=writer)
+
+# fig, ax = plt.subplots()
+# anim = animation.FuncAnimation(fig, make_vel_hist, frames=len(times))
+# writer = animation.FFMpegWriter(fps=len(gasvelocities)/10.)
+# anim.save("./figures/vel_hist.mp4", writer=writer)
+
+plot_avg_positions()
+plot_avg_velocities()
+plot_energies()
+plot_relative_energy()
--- src/sph.py
+++ src/sph.py
@@ -19,7 +19,7 @@ from amuse.units import (units, constants, nbody_system)
 from os import getcwd, chdir
 from tqdm import tqdm
 
-np.random.seed(1)
+# np.random.seed(1)
 
 # x = 1|units.parsec
 # t = 0.1|units.Myr
@@ -95,14 +95,6 @@ def onestepplot():
     plt.ylabel("parsec")
     plt.show()
 
-def plot_energies(Us, Ks, Ts):
-    plt.semilogy(Ts, Us, label="potential energy")
-    plt.semilogy(Ts, Ks, label="kinetic energy")
-    plt.xlabel("Time (Myr)")
-    plt.ylabel("Energy (m^2 kg s^-2)")
-    plt.legend()
-    plt.show()
-
 def star_control(bodies, n_stars):
     bodies_pd = pd.DataFrame(np.array(bodies.stellar_type.number), columns=["stellar_type"])
     return list(bodies_pd.value_counts().index[-1])[0]
@@ -112,7 +104,6 @@ def delete_outofbounds(gas):
     selection = gas[mask]
     gas.remove_particles(selection)
     return gas
-
 
 
 
@@ -146,70 +137,73 @@ def print_info(gravity_initial_total_energy, gravity, hydro, gas, i, start_mass,
 
 
 # print(dir(gas))
-def gravity_hydro_bridge(gravity, hydro, gravhydro, evolution, wind, channel, bodies, gas, t_end, dt, dt_bridge, n_stars):
-    dt_SN = 0.01 | units.Myr
+def gravity_hydro_bridge(gravity, hydro, gravhydro, evolution, wind, channel, bodies, gas, t_end, dt, dt_bridge, n_stars, gasmass, run):
+    # dt_SN = 0.01 | units.Myr
     t_steps = np.arange(0, t_end.value_in(units.Myr), dt.value_in(units.Myr)) | units.Myr
-    # onestepplot()
 
     Us = []
     Ks = []
     Ts = []
+    gas_oldpos = []
+    gas_oldvel = []
+    stars_oldpos = []
+    stars_oldvel = []
 
-    oldpos = []
-    oldvel = []
 
     for i, t in enumerate(tqdm(t_steps)):
         gravity, hydro, gravhydro, evolution, wind, bodies, gas = simulate(gravity, hydro, gravhydro, evolution, wind, channel, bodies, gas, t)
 
         U = bodies.potential_energy() + gas.potential_energy()
         K = bodies.kinetic_energy() + gas.kinetic_energy()
+
         Us.append(abs(U.value_in(units.m**2 * units.kg * units.s**-2)))
         Ks.append(abs(K.value_in(units.m**2 * units.kg * units.s**-2)))
         Ts.append(t.value_in(units.Myr))
-
-        oldpos.append(gas.position)
-        oldvel.append(gas.velocity)
+        gas_oldpos.append(gas.position)
+        gas_oldvel.append(gas.velocity)
+        stars_oldpos.append(bodies.position)
+        stars_oldvel.append(bodies.velocity)
 
         # print_info(gravity_initial_total_energy, gravity, hydro, gas, i, start_mass, bodies, t)
 
         if star_control(bodies, n_stars) == 4:
-            t_SN = t
-            print(t_SN)
-            hydro.parameters.timestep = dt_SN / 2
-            gravhydro.timestep = dt_SN/4
-            # plot_energies(Us, Ks, Ts)
-            # onestepplot()
-            break
+            hydro.parameters.timestep = 0.0004 | units.Myr #400 jaar
+            gravhydro.timestep = 0.0002 | units.Myr
     
-    t_steps_supernova = np.arange(t_SN.value_in(units.Myr), t_SN.value_in(units.Myr) + 3, dt_SN.value_in(units.Myr)) | units.Myr
-    for i, t in enumerate(tqdm(t_steps_supernova)):
-        gravity, hydro, gravhydro, evolution, wind, bodies, gas = simulate(gravity, hydro, gravhydro, evolution, wind, channel, bodies, gas, t)
+    # t_steps_supernova = np.arange(t_SN.value_in(units.Myr), t_SN.value_in(units.Myr) + 2, dt_SN.value_in(units.Myr)) | units.Myr
+    # for i, t in enumerate(tqdm(t_steps_supernova)):
+    #     gravity, hydro, gravhydro, evolution, wind, bodies, gas = simulate(gravity, hydro, gravhydro, evolution, wind, channel, bodies, gas, t)
 
         
-        U = bodies.potential_energy() + gas.potential_energy()
-        K = bodies.kinetic_energy() + gas.kinetic_energy()
-        Us.append(abs(U.value_in(units.m**2 * units.kg * units.s**-2)))
-        Ks.append(abs(K.value_in(units.m**2 * units.kg * units.s**-2)))
-        Ts.append(t.value_in(units.Myr))
+    #     U = bodies.potential_energy() + gas.potential_energy()
+    #     K = bodies.kinetic_energy() + gas.kinetic_energy()
 
-        oldpos.append(gas.position)
-        oldvel.append(gas.velocity)
+    #     Us.append(abs(U.value_in(units.m**2 * units.kg * units.s**-2)))
+    #     Ks.append(abs(K.value_in(units.m**2 * units.kg * units.s**-2)))
+    #     Ts.append(t.value_in(units.Myr))
+    #     gas_oldpos.append(gas.position)
+    #     gas_oldvel.append(gas.velocity)
+    #     stars_oldpos.append(bodies.position)
+    #     stars_oldvel.append(bodies.velocity)
 
         # print_info(gravity_initial_total_energy, gravity, hydro, gas, i, start_mass, bodies, t)
 
 
-    
-    plot_energies(Us, Ks, Ts)
-    # onestepplot()
+
     
 
     evolution.stop()
     gravity.stop()
     hydro.stop()
 
-    np.save("./positions.npy", oldpos)
-    np.save("./velocities.npy", oldvel)
-    np.save("./times.npy", Ts)
+    np.save("./data/potential_energy_ratio{}_run{}.npy".format(gasmass, run), Us)
+    np.save("./data/kinetic_energy_ratio{}_run{}.npy".format(gasmass, run), Ks)
+    np.save("./data/times_ratio{}_run{}.npy".format(gasmass, run), Ts)
+    np.save("./data/gaspositions_ratio{}_run{}.npy".format(gasmass, run), gas_oldpos)
+    np.save("./data/gasvelocities_ratio{}_run{}.npy".format(gasmass, run), gas_oldvel)
+    np.save("./data/starpositions_ratio{}_run{}.npy".format(gasmass, run), stars_oldpos)
+    np.save("./data/starvelocities_ratio{}_run{}.npy".format(gasmass, run), stars_oldvel)
+    
     
 
 def init_stars(n_stars=10, alpha_IMF=-2.35):
@@ -222,11 +216,11 @@ def init_stars(n_stars=10, alpha_IMF=-2.35):
     print("max mass star:", m_stars[np.argmax(m_stars)])
     return m_stars
 
-def main():
+def main(gasmass, run):
     dt = 0.1 | units.Myr
     dt_winds = 0.05 | units.Myr
-    dt_hydro = 0.01 | units.Myr
-    dt_bridge = 0.05 | units.Myr  #1.0*Pinner
+    dt_hydro = 0.04 | units.Myr
+    dt_bridge = 0.02 | units.Myr  #1.0*Pinner
     n_stars=10
     
     m_stars = init_stars(n_stars)
@@ -245,7 +239,7 @@ def main():
 
     ## Maak gasdeeltjes
     Ngas = 1000  # 10000
-    total_gas_mass = 5*total_mass
+    total_gas_mass = gasmass*total_mass
     gasconverter=nbody_system.nbody_to_si(total_gas_mass+total_mass,r_cluster)
     gas = new_plummer_gas_model(Ngas, convert_nbody=gasconverter)
     # gas.scale_to_standard(gasconverter)
@@ -265,9 +259,6 @@ def main():
     hydro.parameters.gas_epsilon = eps
     hydro.parameters.sph_h_const = eps
 
-
-    # Ngas = 10000
-    # gas = new_plummer_gas_model(Ngas, convert_nbody=converter) #Note: this is virialized gas, so it has velocities
     hydro.particles.add_particles(gas)
     Mgas = np.sum(gas.mass)
     mgas = Mgas/Ngas
@@ -284,10 +275,6 @@ def main():
     # Stellar evolution
     evolution = SeBa()
     evolution.particles.add_particles(bodies)
-    ch_e2g = evolution.particles.new_channel_to(gravity.particles)
-    ch_g2e = gravity.particles.new_channel_to(bodies) # Unnecessary?
-    ch_e2b = evolution.particles.new_channel_to(bodies)
-    ch_e2b.copy()
 
     # Stellar wind  p. 223
     wind = new_stellar_wind(mgas, target_gas=gas, timestep=dt_winds, derive_from_evolution=True)
@@ -304,13 +291,18 @@ def main():
                 }
 
     t_end = 10.0 | units.Myr
-    gravity_hydro_bridge(gravity, hydro, gravhydro, evolution, wind, channel, bodies, gas, t_end, dt, dt_bridge, n_stars)
+    gravity_hydro_bridge(gravity, hydro, gravhydro, evolution, wind, channel, bodies, gas, t_end, dt, dt_bridge, n_stars, gasmass, run)
     return 0
 
 
 if __name__ == "__main__":
     fix_cwd()
-    exit(main())
+    # runs_per_gasmass = np.arange(0, 10)
+    # gas_mass_ratios = [1, 5, 10, 15, 20, 25]
+    # for gasmass in gas_mass_ratios:
+    #     for run in runs_per_gasmass:
+    #         main(gasmass, run)
+    exit(main(5, "test"))
 
 
 # Interessant boek? https://misaladino.com/wp-content/uploads/2019/11/Thesis_Martha_Irene.pdf
